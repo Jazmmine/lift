df028bb422ed85687495d3bb1cebb02ff97903ce
Jazmmine <jazmine.angoma@gmail.com>
2016-10-12 18:27:35 -0500

    subiendo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
   $(document).ready(function() {
-    $('select').material_select();
+    $("select").material_select(); //materialize
+    $("#celular-numero").keydown(teclas);
+    $("#celular-numero").keyup(longitudNumero);
+
+    function teclas(evento){
+    	var ascii = evento.keyCode;
+    	if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
+    		return true;
+    	}else{
+    		return false;
+    	}
+    }	
+
+    function longitudNumero(evento){
+    	var longitud = $(this).val().length;
+    	if (longitud == 9) {
+    		$("#btn-next").attr("href", "index.html");
+    	}else{
+    		$("#btn-next").removeAttr("href");
+    	}
+    }
   });
