7a31b7c6e0e4cd594b8f9714f038b7de6cacc696
Jazmmine <jazmine.angoma@gmail.com>
2016-10-13 19:11:10 -0500

    subiendo codigo.html

--- js/app.js
+++ js/app.js
@@ -4,22 +4,20 @@
     $("#celular-numero").keyup(longitudNumero);
     $("#btn-next").click(generarCodigo);
 
-
     function teclas(evento){
     	var ascii = evento.keyCode;
-    	if (ascii != null || ascii == 8 || (ascii >= 48 && ascii <= 57)) {
+    	if (ascii == 8 || (ascii >= 48 && ascii <= 57)) {
+            $("#btn-next").removeAttr("disabled");
     		return true;
     	}else{
-    		//alert("ingresa tu numero celular");
-    		//deshabilita el boton
-    		$("#btn-next").removeAttr("disabled");
+    		alert("ingresar digitos");
     		return false;
     	}
     }	
 
     function longitudNumero(evento){
-    	var longitud = $(this).val().length;
-    	if (longitud == 9) {
+    	var $longitud = $(this).val().length;
+    	if ($longitud == 9) {
     		$("#btn-next").attr("href", "codigo.html");
     	}else{
     		$("#btn-next").removeAttr("href");
@@ -27,8 +25,9 @@
     }
 
     function generarCodigo(evento){
-    	var longitudNumeroCelu = document.getElementById("celular-numero").value;
-    	if (longitudNumeroCelu.length == 9) {
+    	//var longitudNumeroCelu = document.getElementById("celular-numero").value;
+        var $longitudNumeroCelu = $("#celular-numero").val();
+    	if ($longitudNumeroCelu.length == 9) {
 	    	var numeroRandom = Math.round(Math.random()*899)+100;
 	    	var codigoLetra = "LAB-";
 	    	var codigoCompleto = codigoLetra + numeroRandom;
