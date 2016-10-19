b1ef1c45caabb1198e2ae471d69b7806a37dde75
Jazmmine <jazmine.angoma@gmail.com>
2016-10-19 18:00:14 -0500

    subiendo direccion correcta

--- js/mapa.js
+++ js/mapa.js
@@ -6,7 +6,6 @@ var cargarPagina = function() {
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     }
   );
-
 	if (navigator.geolocation) { 
 		// también se puede usar if ("geolocation" in navigator) {}
 		navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
@@ -16,8 +15,24 @@ var cargarPagina = function() {
 var funcionExito = function(posicion) {
 	var lat = posicion.coords.latitude;
     var lon = posicion.coords.longitude;
-    var latlon = new google.maps.LatLng(lat, lon)
-    $("#ubicacion").val("Latitud: " + lat + "Longitud: " + lon);
+    var dir = "";
+    var latlon = new google.maps.LatLng(lat, lon);
+        geocoder = new google.maps.Geocoder();
+        geocoder.geocode({"latLng": latlon}, function(results, status){
+                if (status == google.maps.GeocoderStatus.OK){
+                    if (results[0]){
+                        dir = "Dirección: " + results[0].formatted_address;
+                    }
+                    else{
+                        dir = "No se ha podido obtener ninguna dirección en esas coordenadas.";
+                    }
+                }
+                else{
+                    dir = "El Servicio de Codificación Geográfica ha fallado con el siguiente error: " + status + ".";
+                }
+
+                $("#ubicacion").val(dir);           
+        });
    /* var mapa = document.getElementById("mapa")
     mapa.style.height = "250px";
     mapa.style.width = "500px";*/
